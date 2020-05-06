7177a8e0adc6d8a575c918bea38516f6d15f5f76
vitaliidemidov <demidov1@i.ua>
2020-05-07 02:29:46 +0300

    Updates

---
+++ precache-manifest.359faaef69bd53cd9785fdda3b7f0170.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c1dae94ecc6c46e4c75b32389ecaaf",
+    "url": "/Budget/index.html"
+  },
+  {
+    "revision": "9b20e7eb07687ec9d60a",
+    "url": "/Budget/static/css/main.3415e91b.chunk.css"
+  },
+  {
+    "revision": "772cf3d28acae50f53a7",
+    "url": "/Budget/static/js/2.68d3e858.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Budget/static/js/2.68d3e858.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b20e7eb07687ec9d60a",
+    "url": "/Budget/static/js/main.7b0b4743.chunk.js"
+  },
+  {
+    "revision": "a15df4c37e7eb7aca3ac",
+    "url": "/Budget/static/js/runtime-main.4658c51b.js"
+  }
+]);
